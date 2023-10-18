43ec888d25b20bd902a53085f74af5c59749ca6f
Nicholas <nick.nogue28@outlook.com>
2023-10-17 22:53:24 -0300

    fix FindBank function

--- infrastructure/repository/pix.go
+++ infrastructure/repository/pix.go
@@ -52,17 +52,15 @@ func (r PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
 	if account.ID == "" {
 		return nil, fmt.Errorf("no account found")
 	}
-
 	return &account, nil
 }
 
 func (r PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
 	var bank model.Bank
-	r.Db.Preload("Bank").First(&bank, "id = ?", id)
+	r.Db.First(&bank, "id = ?", id)
 
 	if bank.ID == "" {
 		return nil, fmt.Errorf("no bank found")
 	}
-
 	return &bank, nil
 }
