909e911a16da5ea9b82ba020f745f989a69226fc
Nicholas <nick.nogue28@outlook.com>
2023-10-22 10:24:30 -0300

    add pix-key resource

--- nestjs/src/pix-keys/pix-keys.module.ts
+++ nestjs/src/pix-keys/pix-keys.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
 import { PixKeysService } from './pix-keys.service';
 import { PixKeysController } from './pix-keys.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PixKey } from './entities/pix-key.entity';
-import { BankAccount } from 'src/bank-accounts/entities/bank-account.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from '../bank-accounts/entities/bank-account.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PixKey, BankAccount])],
+  imports: [
+    TypeOrmModule.forFeature([PixKey, BankAccount]),
+    ClientsModule.registerAsync([
+      {
+        name: 'PIX_PACKAGE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.get('GRPC_URL'),
+            package: 'github.com.nichol20.codepix',
+            protoPath: [join(__dirname, 'proto/pix.proto')],
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [PixKeysController],
   providers: [PixKeysService],
 })
