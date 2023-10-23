5830dbd996897c6b8c0969d60c933bc02a3359b3
Nicholas <nick.nogue28@outlook.com>
2023-10-23 11:52:45 -0300

    add colors and background image

--- nextjs/src/components/ThemeRegistry/theme.ts
+++ nextjs/src/components/ThemeRegistry/theme.ts
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -10,9 +11,43 @@ const roboto = Roboto({
 const theme = createTheme({
   palette: {
     mode: "light",
+    primary: {
+      main:
+        process.env.NEXT_PUBLIC_BANK_CODE === "001" ? blue[500] : green[500],
+      light:
+        process.env.NEXT_PUBLIC_BANK_CODE === "001" ? blue[300] : green[300],
+      dark:
+        process.env.NEXT_PUBLIC_BANK_CODE === "001" ? blue[700] : green[700],
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "#707070",
+    },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: (themeParam) => ({
+        body: {
+          backgroundColor: "#f5f5f5",
+          background: 'url("/img/back.png") repeat right top',
+          WebkitBackgroundSize: "cover",
+          MozBackgroundSize: "cover",
+          OBackgroundSize: "cover",
+          backgroundSize: "cover",
+        },
+      }),
+    },
+    //@ts-expect-error - MuiDataGrid is not part of the types
+    MuiDataGrid: {
+      styleOverrides: {
+        columnHeaderTitle: {
+          fontWeight: "bold",
+        },
+      },
+    },
   },
 });
 
