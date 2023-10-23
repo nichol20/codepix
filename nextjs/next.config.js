8d20042ea2cd18c77a85489f35fed846259bcf5f
Nicholas <nick.nogue28@outlook.com>
2023-10-23 11:53:11 -0300

    update next config

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    distDir:
+      process.env.NODE_ENV !== "production"
+        ? `.${process.env.NEXT_PUBLIC_BANK_CODE}-next`
+        : ".next",
     experimental: {
-        serverActions: true
+      serverActions: true,
     },
     async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/bank-accounts',
-                permanent: false
-            }
-        ]
-    }
-}
-
-module.exports = nextConfig
+      return [
+        {
+          source: "/",
+          destination: "/bank-accounts",
+          permanent: false,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
