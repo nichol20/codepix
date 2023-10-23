c734ccda850f3027bc6047f44540e247a150b6a7
Nicholas <nick.nogue28@outlook.com>
2023-10-22 21:35:49 -0300

    add CardAction component

---
+++ nextjs/src/components/CardAction.tsx
@@ -0,0 +1,30 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { PropsWithChildren } from "react";
+import { Card } from "./Card";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForwardIos";
+import { Button, SxProps, Theme } from "@mui/material";
+
+export type CardActionProps = {
+    sx?: SxProps<Theme>;
+};
+
+export function CardAction(props: PropsWithChildren<CardActionProps>) {
+    return (
+        <Card>
+            <Grid2 container spacing={2}>
+                <Grid2 xs={12} sm={9} sx={props.sx}>{props.children}</Grid2>
+                <Grid2
+                    xs={12}
+                    sm={3}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"flex-end"}
+                >
+                    <Button color='primary'>
+                        <ArrowForwardIcon />
+                    </Button>
+                </Grid2>
+            </Grid2>
+        </Card>
+    );
+}
