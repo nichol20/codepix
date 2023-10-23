aa69246e0c61510f89e51e896e583f9492549fee
Nicholas <nick.nogue28@outlook.com>
2023-10-23 10:22:25 -0300

    add env

--- nextjs/src/queries/get-bank-account.query.ts
+++ nextjs/src/queries/get-bank-account.query.ts
@@ -4,7 +4,7 @@ export async function getBankAccount(
   bankAccountId: string
 ): Promise<BankAccount> {
   const response = await fetch(
-    `${"http://host.docker.internal:3000"}/bank-accounts/${bankAccountId}`,
+    `${process.env.NEXT_PUBLIC_NEST_API_URL}/bank-accounts/${bankAccountId}`,
     {
       next: {
         revalidate: 20,
